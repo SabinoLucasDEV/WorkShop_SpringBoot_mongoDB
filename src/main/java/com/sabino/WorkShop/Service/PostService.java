d7d6c32b2440f8373174af23d896221f068822c9
N00bM4T3R <sabinolbn1810@gmail.com>
2025-06-11 12:04:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/sabino/WorkShop/Service/PostService.java
@@ -0,0 +1,30 @@
+package com.sabino.WorkShop.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sabino.WorkShop.Domain.Post;
+import com.sabino.WorkShop.Domain.User;
+import com.sabino.WorkShop.Repository.PostRepository;
+import com.sabino.WorkShop.Service.Exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	public PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	
+}
