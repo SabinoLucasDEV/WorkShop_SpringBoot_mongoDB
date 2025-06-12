a18f0c216f7db81b35ef5b6bee2caf7c3ffbe4b6
N00bM4T3R <sabinolbn1810@gmail.com>
2025-06-12 09:36:26 -0300

    Consulta simples com query methods

--- src/main/java/com/sabino/WorkShop/Resource/PostResource.java
+++ src/main/java/com/sabino/WorkShop/Resource/PostResource.java
@@ -1,13 +1,17 @@
 package com.sabino.WorkShop.Resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sabino.WorkShop.Domain.Post;
+import com.sabino.WorkShop.Resource.util.URL;
 import com.sabino.WorkShop.Service.PostService;
 
 @RestController
@@ -26,7 +30,14 @@ public class PostResource {
 		
 		
 	}
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> post = service.findByTitle(text);
+		return ResponseEntity.ok().body(post);
+		
+		
 	
 	
 
-}
+	}}
